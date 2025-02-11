035bdf11a6f46a53d9ff6c6084bd54d8c12fdb96
Nahidul Islam <nahidulaiub@gmail.com>
2025-02-11 12:02:14 +0600

    update

---
+++ naming.js
@@ -0,0 +1,35 @@
+//1 .no keyword in variable name 
+ var false = 45;
+ var var = 50;
+
+ //2. no space or gap in variable name
+
+ var is happy = false;
+ var is_happy = true;
+
+ //3. no quote
+
+ var "address" = " narayangonje ";
+
+ //4.no can not start with a number but number orher then the first  letter is allowed
+
+ var money =45;
+ var money2 = 55;
+ var money3 = 88;
+
+ //5. uname is case  sensitive 
+
+ address
+ address
+ ADDRESS 
+addRess 
+ADDress 
+
+
+// 6. how to write  a long  variable  name
+
+var my current home address = "Cumilla";
+var mycurrenthomeaddress = "cumilla";
+var my_current_home_address = "Cumilla"; // snake case
+var myCurrentHomeAddress = "cumilla"; // camel case
+var MyCurrentHomeAddress = "cumilla"; // Pascal cas
